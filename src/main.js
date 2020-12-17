7698702deadd860054bb89a8b4e5fcd4cb57a970
Bryan Solano <bryantrabajosu@gmail.com>
2020-12-17 16:40:20 -0600

    routing init data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuelidate from 'vuelidate'
+import router from './router'
 
 Vue.use(vuelidate)
 Vue.config.productionTip = false
 
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
